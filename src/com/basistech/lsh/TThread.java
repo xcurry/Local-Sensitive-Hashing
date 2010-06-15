380949e3ad0d8224f7a8aa526f0908bb6b07d4f5
jwp <jwp@81b6c55f-58c9-4729-b236-40b2a262f43e>
2010-06-15 22:31:49 +0000

    need global tf counts, add tmrw
    git-svn-id: http://svn.basistech.net/engineering/u/jwp/lshj@13748 81b6c55f-58c9-4729-b236-40b2a262f43e

--- src/com/basistech/lsh/TThread.java
+++ src/com/basistech/lsh/TThread.java
@@ -48,32 +48,14 @@ public class TThread {
         if(hasEntropy){
             return entropy;
         }
-//        FeatureVector fv = new FeatureVector();
-//        double total=0;
-//        for(Tweet t: tweets){
-//            total+=fv.merge(t.getFeatures());
-//        }
-//        entropy=0;
-//        for(double d: fv.values()){
-//            entropy-=d/total*Math.log(d/total);
-//        }
-        HashMap<String, Integer> tf = new HashMap<String, Integer>();
-        double totalCount = 0.0d;
+        FeatureVector fv = new FeatureVector();
+        double total=0;
         for(Tweet t: tweets){
-            String text = t.getText();
-            for (String tok : text.split("\\s+")) {
-                Integer count = tf.get(tok);
-                if (count == null) {
-                    count = 0;
-                }
-                tf.put(tok, count + 1);
-                ++totalCount;
-            }
+            total+=fv.merge(t.getFeatures());
         }
         entropy=0;
-        for (Integer count : tf.values()) {
-            double p = count / totalCount;
-            entropy-= p * Math.log(p);
+        for(double d: fv.values()){
+            entropy-=d/total*Math.log(d/total);
         }
         entropy/=LOG2;
         hasEntropy = true;
