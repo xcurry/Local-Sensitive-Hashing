89164d122e1ec8f060bfe9d5f602a69caf28fe73
jwp <jwp@81b6c55f-58c9-4729-b236-40b2a262f43e>
2010-06-18 21:45:19 +0000

    rename suffix
    git-svn-id: http://svn.basistech.net/engineering/u/jwp/lshj@13834 81b6c55f-58c9-4729-b236-40b2a262f43e

--- scripts/pipelines.mk
+++ scripts/pipelines.mk
@@ -1,8 +1,7 @@
-all: threads2.ent_sz
 
-%.sorted: %.log
+%.grouped: %.all_threads
 	sort -k2,2 < $< > $@
 
-%.ent_sz: %.sorted
+%.ent_sz: %.grouped
 	perl add-entropy-size.perl < $< | sort -k2,2gr -k1,1gr > $@
 
