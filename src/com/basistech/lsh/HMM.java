f0d7560587987f7c4a2b753a0b96dc196809062b
jwp <jwp@81b6c55f-58c9-4729-b236-40b2a262f43e>
2010-06-23 13:56:14 +0000

    hmm, close
    git-svn-id: http://svn.basistech.net/engineering/u/jwp/lshj@14051 81b6c55f-58c9-4729-b236-40b2a262f43e

--- src/com/basistech/lsh/HMM.java
+++ src/com/basistech/lsh/HMM.java
@@ -1,7 +1,11 @@
 package com.basistech.lsh;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
@@ -19,7 +23,8 @@ public class HMM {
         public State(int id, Set<String> syms) {
             this(id);
             for (String sym : syms) {
-                addSymbol(sym, Math.abs(rng.nextInt()));                
+                // TODO: good dist?
+                addSymbol(sym, Math.abs(rng.nextInt() % 4) + 1);                
             }
             normalize();
         }
@@ -47,37 +52,87 @@ public class HMM {
         }         
 
         public String toString() {
-            String s = "state_" + id + " = {";
+            String s = id + " = {";
             for (Entry<String, Double> e : observations.entrySet()) {
                 s += e.getKey() + " " + String.format("%.4f, ", e.getValue());
             }            
             s += "}";
             return s;
         }
-        
-        public int hashCode() {
-            return id;
+    }
+
+    public class Trellis {
+        public Trellis(int length, int height) {
+            this.length = length;
+            this.height = height;
+            rep = new double[length][height];
         }
+        public void extend(int length) {
+            if (this.length >= length) {
+                return;
+            }
+            int prevLength = rep.length;
+            rep = Arrays.copyOf(rep, length);
+            for (int i = prevLength; i < length; ++i) {
+                rep[i] = new double[height];
+            }
+            this.length = length;
+        } 
+        public double get(int row, int col) {
+            if (row < length && col < height) {
+                return rep[row][col];
+            }
+            return 0.0d;
+        }
+        public boolean put(int row, int col, double value) {
+            if (row < length && col < height) {
+                rep[row][col] = value;
+                return true;
+            }
+            return false;
+        }
+        public String toString() {
+            String s = new String();
+            if (rep != null) {
+                for (int state = height - 1; state > -1; --state) {
+                    for (int time = 0; time < length; ++time) {
+                        s += String.format("%.4f", rep[time][state]) + " ";
+                    }
+                    s += "\n";
+                }
+            }            
+            return s;
+        }
+        public int length;
+        public int height;
+        public double[][] rep;
     }
     
     private double[][] transitions;
-    private Set<State> states;
+    private List<State> states;
+    private int nStates;
+    private Trellis alpha;
+    private Trellis beta;
     private static Random rng;
     static {
-        rng = new Random(); 
+        rng = new Random(5); 
     }
 
     public HMM(int nStates, Set<String> observations) {
         initializeTransitions(nStates);
         initializeStates(nStates, observations);
+        int nObs = observations.size();
+        this.nStates = nStates; 
+        alpha = new Trellis(5, nObs);
+        beta = new Trellis(5, nObs);
     }
 
     private void initializeStates(int nStates, Set<String> observations) {
-        states = new HashSet<State>();
+        states = new ArrayList<State>();
         for (int i = 0; i < nStates; ++i) {
             State s = new State(i, observations);
-            states.add(s);            
-        }        
+            states.add(s);
+        }
     }
 
     private void initializeTransitions(int nStates) {
@@ -85,7 +140,8 @@ public class HMM {
         for (int i = 0; i < nStates; ++i) {
             double sum = 0.0d;
             for (int j = 0; j < nStates; ++j) {
-                int v = Math.abs(rng.nextInt());
+                // TODO: good dist?
+                int v = Math.abs(rng.nextInt() % 4) + 1; 
                 transitions[i][j] = v;
                 sum += v;
             }
@@ -95,16 +151,50 @@ public class HMM {
         }
     }
 
-    public String toString() {
-        String s = "States:\n";
-        for (State st : states) {
-            s += "  " + st.toString() + "\n";            
+    private void printAB() {
+        System.out.println("a:\n" + alpha);
+        System.out.println("b:\n" + beta);
+    }
+    
+    public double forward(List<String> obs) {
+        int obsLength = obs.size();
+        alpha.extend(obsLength);
+        int time = -1;
+        for (String sym : obs) {
+            ++time;
+            // initalization
+            if (time == 0) {
+                for (State s : states) {
+                    double obsP = s.getProb(sym);
+                    alpha.put(time, s.id, obsP);
+                }
+                continue;
+            }
+            // induction
+            for (State currentState : states) {
+                int current = currentState.id;
+                double incomingAlpha = 0.0d;
+                for (int previous = 0; previous < nStates; ++previous) {
+                    double transP = transitions[previous][current];
+                    incomingAlpha += transP * alpha.get(time - 1, previous);
+                }
+                double obsP = currentState.getProb(sym);
+                alpha.put(time, current, obsP * incomingAlpha);
+            }
         }
-        
-        s += "Transitions: \n";
+        double totalAlpha = 0.0d;
+        for (State s : states) {
+            int i = s.id;
+            totalAlpha += alpha.get(time, i);
+        }
+        return totalAlpha;
+    }
+
+    public String toString() {
+        String s = "States:\n" + states.toString() + "\n";
+        s += "Transitions:\n";
         int nStates = states.size();
         for (int i = 0; i < nStates; ++i) {
-            s += "  ";
             for (int j = 0; j < nStates; ++j) {
                 s += String.format("%.4f", transitions[i][j]) + " ";
             }
@@ -120,6 +210,17 @@ public class HMM {
         obsSyms.add("c");
         HMM h = new HMM(3, obsSyms);
         System.out.println(h.toString());
+
+        List<String> obs = new LinkedList<String>();
+        obs.add("b");
+        obs.add("b");
+        obs.add("c");
+        //h.printAB();
+        //System.out.println("=== resize ===");
+        double totalAlpha = h.forward(obs);
+        h.printAB();
+        System.out.println("* a_tot = " + totalAlpha);
+    
     }
 
     public static void main(String[] args) {
